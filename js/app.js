2455a8ce6e2972413c68930c485ddeb57f388aab
Logan Rice <loganrice72@gmail.com>
2014-06-04 10:29:15 -0700

    click to add item test

--- js/app.js
+++ js/app.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	$(".tasks").on("click", "li", function() {
 		$(this).toggleClass("done");
 	})
+
+	$(".add").click(function() {
+		$("ul:first-child").prepend("<li>Test</li>");
+		// $('.list').css("background", "black");
+	})
 })
